cbef953aee19d4d43720373c4aa31457d7e48685
Lew619 <lew619@hotmail.co.uk>
2025-02-15 10:52:18 +0000

    Added Go solution 2015 pt1 + pt2

--- 2015/01_aoc/main1.go
+++ 2015/01_aoc/main1.go
@@ -1,9 +1,48 @@
-package main1
+package main
 
 import (
 	"fmt"
+	"os"
+	"log"
 )
 
 func main() {
+	data, err := os.ReadFile("/Users/lf16/Documents/GitHub/Advent_of_Code/puzzle_inputs/2015/01_aoc_2015.txt")
+	// data := aoc1read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// var length = len(data)
+	var UpCount int = 0
+	var DownCount int = 0
+	var RunningCount int = 0 // Part 2 
+	var FirstBasement bool = false
+
+	for pos, char := range string(data) {
+
+		if char == '(' {
+			UpCount += 1
+			RunningCount += 1
+		}
+
+		if char == ')' {
+			DownCount += 1
+			RunningCount -= 1
+		}
+
+		if RunningCount == -1 && FirstBasement == false {
+			var FirstBasementPos int = pos + 1 
+			fmt.Printf("First basement -1 position: %d\n", FirstBasementPos)
+			FirstBasement = true
+		}
+		// fmt.Println(pos)
+
 	
+	}
+	fmt.Printf("Up floors: %d\nDown floors: %d\n", UpCount, DownCount)
+
+	var FloorStart int = 0
+	CurrentFloor := FloorStart - DownCount + UpCount 
+	fmt.Printf("Current Floor is: %d\n", CurrentFloor)
 }
